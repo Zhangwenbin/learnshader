77438449859cba1ba5c3e996009974ed60d4821a
zwb <991355745@qq.com>
2019-08-08 20:45:49 +0800

    屏幕特效 post

---
+++ Assets/zCustomShaders/Shaders/1self/posteffect/post1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class post1 : MonoBehaviour {
+
+    public RenderTexture dstrt;
+    public Material mat;
+    public Material displaymat;
+    private void Start ( )
+        {
+        dstrt = new RenderTexture ( Screen.width, Screen.height, 16 );
+        displaymat.mainTexture = dstrt;
+
+        }
+    private void OnRenderImage ( RenderTexture source, RenderTexture destination )
+        {
+        Graphics.Blit ( source, dstrt, mat );
+        Graphics.Blit ( dstrt, destination );
+        }
+    }
