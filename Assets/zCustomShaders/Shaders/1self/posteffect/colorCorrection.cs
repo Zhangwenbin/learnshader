ad84abf122178040528e2e9eefbee4a48d857bbc
zwb <991355745@qq.com>
2019-08-09 17:16:28 +0800

    色彩增强

---
+++ Assets/zCustomShaders/Shaders/1self/posteffect/colorCorrection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorCorrection : MonoBehaviour {
+    public AnimationCurve rc,gc,bc;
+    private Material mat;
+    private Texture2D rgbTex;
+    public Shader shader;
+	// Use this for initialization
+	void Start () {
+        mat = new Material ( shader );
+        rgbTex = new Texture2D ( 256, 4, TextureFormat.ARGB32, false );
+        rgbTex.hideFlags = HideFlags.HideAndDontSave;
+        rgbTex.wrapMode = TextureWrapMode.Clamp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for ( float i = 0; i < 1; i+=1/255f )
+            {
+            float rch=Mathf.Clamp01(rc.Evaluate(i));
+            float gch=Mathf.Clamp01(gc.Evaluate(i));
+            float bch=Mathf.Clamp01(bc.Evaluate(i));
+            rgbTex.SetPixel ( Mathf.FloorToInt ( i * 255 ), 0, new Color ( rch, rch, rch ) );
+            rgbTex.SetPixel ( Mathf.FloorToInt ( i * 255 ), 1, new Color ( gch, gch, gch ) );
+            rgbTex.SetPixel ( Mathf.FloorToInt ( i * 255 ), 2, new Color ( bch, bch, bch ) );
+            rgbTex.Apply ( );
+            }        
+		
+	}
+
+    private void OnRenderImage ( RenderTexture source, RenderTexture destination )
+        {
+        mat.SetTexture ( "rgbtex", rgbTex );
+        Graphics.Blit ( source,destination, mat );
+        }
+    }
---
+++ Assets/zCustomShaders/Shaders/1self/posteffect/colorcorrection.shader
@@ -0,0 +1,60 @@
+﻿Shader "zwb/post/colorcorrection"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			float4 _MainTex_TexelSize;
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv=v.uv;
+			
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			sampler2D rgbtex;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				float red=tex2D(rgbtex,half2(col.r,0.5/4)).rgb*fixed3(1,0,0).r;
+				float green=tex2D(rgbtex,half2(col.g,1.5/4)).rgb*fixed3(0,1,0).g;
+				float blue=tex2D(rgbtex,half2(col.b,2.5/4)).rgb*fixed3(0,0,1).b;
+
+				return float4(red,green,blue,col.a);
+
+				
+			}
+			ENDCG
+		}
+	}
+}
